7ba9e02bc47ed8a61825c1cfdee03d3bd9c92a93
Julian Sandoval <juliangodina17@gmail.com>
2026-05-10 21:03:23 -0700

    Adicion de Registro de usuarios

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app')
+
+@section('title')
+    Registrate en Devstagram
+@endsection
+
+@section('contenido')
+    <div class="md:flex md:justify-center md:gap-10 md:items-center">
+        <div class="md:w-6/12 p-5">
+            <img src="{{ asset('img/registrar.jpg') }}" alt="Imagen Registro de Usuarios">
+        </div>
+
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+            <form action="">
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">Nombre</label>
+                    <input 
+                        id="name"
+                        name="name"
+                        type="text"
+                        placeholder="Tu Nombre"
+                        class="p-3 w-full rounded-lg border border-gray-300"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">Nombre de Usuario</label>
+                    <input 
+                        id="username"
+                        name="username"
+                        type="text"
+                        placeholder="Tu Nombre de Usuario"
+                        class="p-3 w-full rounded-lg border border-gray-300"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">Email</label>
+                    <input 
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="Tu Email de Registro"
+                        class="p-3 w-full rounded-lg border border-gray-300"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">Password</label>
+                    <input 
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Tu Password"
+                        class="p-3 w-full rounded-lg border border-gray-300"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">Repetir Password</label>
+                    <input 
+                        id="password_confirmation"
+                        name="password_confirmation"
+                        type="password"
+                        placeholder="Repite tu Password"
+                        class="p-3 w-full rounded-lg border border-gray-300"
+                    />
+                </div>
+
+                <input type="submit" value="Crear Cuenta" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+
+        <title>Devstagram - @yield('title')</title>
+    </head>
+    <body class="bg-gray-100">
+        <header class="p-5 bg-white shadow">
+            <div class="container mx-auto flex justify-between items-center">
+                <h1 class="text-3xl font-black">
+                    Devstagram
+                </h1>
+
+                <nav class="flex gap-2 items-center">
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="/crear-cuenta">Crear Cuenta</a>
+                </nav>
+            </div>
+        </header>
+
+        <main class="conteiner mx-auto mt-10">
+            <h2 class="font-black text-3xl mb-10 text-center">
+                @yield('title')
+            </h2>
+            @yield('contenido')
+        </main>
+
+        <footer class="text-center p-5 text-gray-500 font-bold uppercase mt-10">
+            Devstagram - Todos los derechos reservados {{ now()->year }}
+        </footer>
+    </body>
+</html>
